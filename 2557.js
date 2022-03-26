f1f3be47fecaa7524f63cb98ffcaf3cc5c280d5c
KIMYUNMEE <jayoo8665@naver.com>
2022-03-26 21:41:51 +0900

    백준 코딩테스트 공부 - 2557번

---
+++ 2557.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+input = input[0];
+input = input.split(' ').map((item) => +item);
+
+solution();
+function solution() {
+    // Write your code
+    console.log('Hello World!');
+}
