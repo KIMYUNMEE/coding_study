a0aeb0d6ad8ebdd98a3ec2c912084cce7356f1aa
KIMYUNMEE <jayoo8665@naver.com>
2022-03-29 22:23:55 +0900

    백준 코딩테스트 공부 - 10926번

---
+++ 10926.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim();
+
+
+solution();
+function solution() {
+    // Write your code
+  
+console.log(`${input}??!`);
+}
+
+
